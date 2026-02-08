6c0032f048b1ab562ce1fe17bc88694b0aa3a422
Elfsong <dumingzhex@gmail.com>
2026-02-08 12:58:04 +0000

    Remove auto_conversion.sh script and update model path in auto_eval_stage_3.sh for consistency with new checkpoint naming convention.

---
+++ auto_conversion_stage_3.sh
@@ -0,0 +1,28 @@
+# Login to Hugging Face
+# hf auth login
+
+source /opt/venv/bin/activate
+export PYTHONPATH="/duyntc2/megatron-bridge/src:/duyntc2/megatron-bridge/3rdparty/Megatron-LM"
+export WANDB_MODE=offline
+
+for step in {1000..10000..500}
+do
+    formatted_step=$(printf "%07d" $step)
+
+    echo "=================================================="
+    echo "Converting checkpoint: iter_${formatted_step}"
+    echo "=================================================="
+
+    # Convert the Nemo checkpoint to HF
+    python -m torch.distributed.run --nproc-per-node=1 /duyntc2/megatron-bridge/examples/conversion/convert_checkpoints.py export \
+        --hf-model /traininguser/megatron-bridge/weights/qwen3_32b \
+        --megatron-path /traininguser/stage_3_training/megatron-bridge/nemo_experiments/qwen3_32b_24_node/checkpoints/iter_${formatted_step} \
+        --hf-path ./iter_${formatted_step}
+
+    if [ $? -eq 0 ]; then
+        hf upload Elfsong/VLM_stage_3_iter_${formatted_step} ./iter_${formatted_step}/
+    else
+        echo "Conversion failed for step $step"
+        exit 1
+    fi
+done
--- auto_eval_stage_2.sh
+++ auto_eval_stage_2.sh
@@ -8,8 +8,8 @@ do
     echo "========================================================"
 
     lm_eval --model vllm \
-        --model_args pretrained=Elfsong/VLM_stage_3_iter_${formatted_step},tensor_parallel_size=1,dtype=bfloat16,gpu_memory_utilization=0.9,data_parallel_size=8 \
+        --model_args pretrained=/home/traininguser/stage_2_training/megatron-bridge/nemo_experiments/qwen3_32b_24_node/checkpoints/iter_${formatted_step},tensor_parallel_size=1,dtype=bfloat16,gpu_memory_utilization=0.9,data_parallel_size=8 \
         --tasks vmlu,vmlu_final,include_base_44_vietnamese,include_base_44_chinese,include_base_44_french,include_base_44_german,include_base_44_japanese,include_base_44_korean,include_base_44_malay,include_base_44_russian,vnhsge,vietnews,viquad,zalo_math,mmlu,lambada,hellaswag,truthfulqa_mc1,truthfulqa_mc2,truthfulqa_gen,arc_easy,arc_challenge,xcopa,belebele_vie_Latn,belebele_zho_Hans,belebele_eng_Latn,belebele_fra_Latn,belebele_jpn_Jpan,belebele_deu_Latn,belebele_zsm_Latn,belebele_kor_Hang,belebele_rus_Cyrl \
         --batch_size auto \
-        --wandb_args project=vlm-eval,name=eval_iter_stage_3_${formatted_step}
+        --wandb_args project=vlm-eval,name=eval_iter_stage_2_${formatted_step},tags=stage_2
 done
